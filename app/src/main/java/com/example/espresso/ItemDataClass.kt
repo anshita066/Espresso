ccf059fc4b920ead082ab4692f146094c95b3a63
anshita066 <anshita_066@yahoo.in>
2021-11-17 08:24:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/espresso/ItemDataClass.kt
@@ -0,0 +1,3 @@
+package com.example.espresso
+
+data class ItemDataClass(val itemName: String, val isFavorite: Boolean)
---
+++ app/src/main/java/com/example/espresso/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.espresso
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var itemViewModel: ViewModel
+    private val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val application = this.application
+        itemViewModel = ViewModelProvider(this).get(ViewModel(application)::class.java)
+        setupUI()
+        setListeners()
+    }
+
+    private fun setListeners() {
+        addToFavorite_BTN.setOnClickListener {
+            if (itemFavorite_CB.isChecked) {
+                itemViewModel.removeFromFavorite()
+                Log.i(TAG, "Unchecked")
+            }
+            else {
+                itemViewModel.saveInFavorite(ItemDataClass(itemName_TV.text.toString(), true))
+                Log.i(TAG, "Checked")
+            }
+        }
+    }
+
+    private fun setupUI() {
+        itemViewModel.isItemFavorite.observe(this, Observer {
+            //Log.i(TAG, it.toString())
+            itemFavorite_CB.isChecked = it
+        })
+    }
+}
